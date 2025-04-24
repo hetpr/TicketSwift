363b613c971d909e74ab1ae80ea7ae0ad6d379cb
Het Prajapati <hetprajapati7865@gmail.com>
2025-04-24 09:53:18 +0530

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: "mongodb+srv://hetpr:het12345@cluster0.oao7qau.mongodb.net/ticketswift?retryWrites=true&w=majority&appName=Cluster0",
+    MongoURI: "<MongoDB_URI>",
 }
